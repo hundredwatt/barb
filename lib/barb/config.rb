f25e6df09598dff91af89769ab115782b889b449
Jason Nochlin <hundredwatt@gmail.com>
2014-07-20 11:08:53 -0400

    No need to provide realm argument here

--- lib/barb/config.rb
+++ lib/barb/config.rb
@@ -1,7 +1,7 @@
 module Barb
   module Config
     def basic_auth(&blk)
-      use Rack::Auth::Basic, 'Barb', &blk
+      use Rack::Auth::Basic, &blk
     end
 
     # delegate to App module's #builder method
