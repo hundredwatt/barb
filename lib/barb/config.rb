25dd13a7a54dba7b94e8fc0120ea5af669fea0cf
Jason Nochlin <hundredwatt@gmail.com>
2014-07-20 11:08:35 -0400

    Explicitly define #use in Config

--- lib/barb/config.rb
+++ lib/barb/config.rb
@@ -3,5 +3,10 @@ module Barb
     def basic_auth(&blk)
       use Rack::Auth::Basic, 'Barb', &blk
     end
+
+    # delegate to App module's #builder method
+    def use(*)
+      super
+    end
   end
 end
